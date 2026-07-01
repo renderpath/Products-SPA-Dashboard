31696270bdb9b10c0c6f247ce54bb56f376b71aa
vera <pvk@cds.guru>
2026-07-01 11:52:26 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "src", "main.tsx"),
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.[contenthash].js",
+        clean: true,
+        publicPath: "/"
+    },
+
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html")
+        })
+    ],
+
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        open: true,
+        hot: true
+    }
+};
